9f3181e6393db6321244718bba843688a215ebfe
Joe Burns <josebur86@gmail.com>
2017-12-30 15:53:45 -0600

    Return json of the feeds.

--- main.go
+++ main.go
@@ -1,13 +1,33 @@
 package main
 
 import (
+    "encoding/json"
     "fmt"
     "net/http"
 )
 
+type Feed struct {
+    ID int `json:"id"`
+    Title string `json:"title"`
+    URL string `json:"url"`
+}
+
+var GlobalFeeds []Feed
+
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello Refeed!")
+
+    GlobalFeeds = append(GlobalFeeds, Feed{1, "Example", "http://www.Example.com/rss"})
+    GlobalFeeds = append(GlobalFeeds, Feed{2, "WhoCares", "http://www.WhoCares.com/rss"})
+    GlobalFeeds = append(GlobalFeeds, Feed{3, "Charlie.com", "http://www.Charlie.com/rss"})
+
+    http.HandleFunc("/feeds", func(w http.ResponseWriter, r *http.Request) {
+        response, err := json.Marshal(GlobalFeeds)
+        if err != nil {
+            fmt.Println("Error marshaling feeds: ", err)
+        }
+
+        w.Header().Set("Content-Type", "text/json")
+        fmt.Fprintf(w, "%s", response)
     })
 
     http.ListenAndServe(":8080", nil)
