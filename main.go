d36f5a0d6553739d9fdfe2b13ee907a1d52db693
Joe Burns <josebur86@gmail.com>
2017-12-30 16:40:51 -0600

    Single Feed response.

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
     "encoding/json"
     "fmt"
     "net/http"
+    "strconv"
 
     "github.com/gorilla/mux"
 )
@@ -23,17 +24,38 @@ func main() {
     GlobalFeeds = append(GlobalFeeds, Feed{3, "Charlie.com", "http://www.Charlie.com/rss"})
 
     router := mux.NewRouter() // TODO(joe): StrictSlash(true)??
-    router.HandleFunc("/feeds", apiHandler)
+    router.HandleFunc("/feeds", AllFeedsHandler)
+    router.HandleFunc("/feeds/{id}", SingleFeedHandler)
 
     http.ListenAndServe(":8080", router)
 }
 
-func apiHandler(w http.ResponseWriter, r *http.Request) {
+func AllFeedsHandler(w http.ResponseWriter, r *http.Request) {
     response, err := json.Marshal(GlobalFeeds)
     if err != nil {
         fmt.Println("Error marshaling feeds: ", err)
+        return
     }
 
     w.Header().Set("Content-Type", "text/json; charset=utf-8")
     fmt.Fprintf(w, "%s", response)
+}
+
+func SingleFeedHandler(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    id, err := strconv.Atoi(vars["id"])
+    if err != nil {
+        fmt.Println("Error parsing feed id: ", err)
+        return
+    }
+
+    w.Header().Set("Content-Type", "text/json; charset=utf-8")
+    for _, feed := range GlobalFeeds {
+        if feed.ID == id {
+            json.NewEncoder(w).Encode(feed)
+            return
+        }
+    }
+
+    fmt.Fprintf(w, "{}"); // TODO(joe): What's the more RESTy response to a request to a feed that doesn't exist.
 }
